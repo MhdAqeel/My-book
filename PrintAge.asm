242dd5320330bee089d4e9ce0ea4682a47f58b1f
Rahuman Noor Mohamed Aqeel <mohamedaqeel1130@gmail.com>
2026-01-01 15:53:54 +0530

    Refactor PrintAge.asm for improved clarity

--- PrintAge.asm
+++ PrintAge.asm
@@ -4,49 +4,40 @@ string2: .asciiz "Mohanathas\n"
 string3: .asciiz "Address: "
 string4: .asciiz "Jaffna\n"
 string5: .asciiz "Age: "
-age: .word 20   # Integer value for age
+age: .word 20  
 
 .text
-.global _start
-_start:
-    jal main    # Jump to the main function
-    li $v0, 10  # Syscall 10 to exit the program
-    syscall     # Execute the exit syscall
  
 main:
     # Print "Name: "
-    la $a0, string1  # Load address of "Name: " into $a0
-    li $v0, 4        # Syscall code for printing a string
-    syscall          # Perform the syscall
+    la $a0, string1 
+    li $v0, 4        
+    syscall       
 
     # Print "Mohanathas"
-    la $a0, string2  # Load address of "Mohanathas\n" into $a0
-    li $v0, 4        # Syscall code for printing a string
-    syscall          # Perform the syscall
+    la $a0, string2 
+    li $v0, 4     
+    syscall          
 
     # Print "Address: "
-    la $a0, string3  # Load address of "Address: " into $a0
-    li $v0, 4        # Syscall code for printing a string
-    syscall          # Perform the syscall
+    la $a0, string3  
+    li $v0, 4        
+    syscall          
 
     # Print "Jaffna"
-    la $a0, string4  # Load address of "Jaffna\n" into $a0
-    li $v0, 4        # Syscall code for printing a string
-    syscall          # Perform the syscall
+    la $a0, string4 
+    li $v0, 4       
+    syscall          
 
     # Print "Age: "
-    la $a0, string5  # Load address of "Age: " into $a0
-    li $v0, 4        # Syscall code for printing a string
-    syscall          # Perform the syscall
+    la $a0, string5  
+    li $v0, 4       
+    syscall         
 
-    # Print the age (20)
-    lw $a0, age      # Load the age value into $a0
-    li $v0, 1        # Syscall code for printing an integer
-    syscall          # Perform the syscall
-
-    # Print a newline after the age
-    li $a0, 0xA      # ASCII value for newline
-    li $v0, 11       # Syscall code for printing a character
-    syscall          # Perform the syscall
-
-    jr $ra  # Return to the caller (end of main)
+    # Print the age (xx)
+    lw $a0, age      
+    li $v0, 1       
+    syscall
+    
+    li $v0, 10
+    syscall
